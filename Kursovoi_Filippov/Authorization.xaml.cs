7504f6be37eaa9ed85104a8996d4e52d3e2588f2
kostyafear203@gmail.com <kostyafear203@gmail.com>
2023-02-23 20:48:24 +1000

    Добавьте файлы проекта.

---
+++ Kursovoi_Filippov/Authorization.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursovoi_Filippov
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userData = AgenstvoNedvezjEntities2.GetContext().Users.FirstOrDefault(x => x.Login == txbLogin.Text && x.Password == psbPassword.Password);
+                if (userData == null)
+                {
+                    MessageBox.Show("Такого пользователя нет!", "Ошибка авторизации!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    Manager.MainFrame.Navigate(new List());
+
+                    MessageBox.Show("Здравствуйте, " + userData.Login + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Ошибка" + Ex.Message.ToString() + "Критическая ошибка приложения!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void btnRegIn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Registration());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new List());
+        }
+    }
+}
---
+++ Kursovoi_Filippov/Registration.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursovoi_Filippov
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Page
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            //проверяем есть ли такой же пользователь
+            if (AgenstvoNedvezjEntities2.GetContext().Users.Count(x => x.Login == txbLogin.Text) > 0)
+            {
+                MessageBox.Show("Пользователь с таким логином есть!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            //если прошли проверку логина, записываем нового пользователя с ролью 2
+            try
+            {
+                Users userData = new Users()
+                {
+                    Login = txbLogin.Text, //получаем даные логина
+                    Password = txbPass.Text, //получаем парол
+                };
+                AgenstvoNedvezjEntities2.GetContext().Users.Add(userData);//добавление объекта
+                AgenstvoNedvezjEntities2.GetContext().SaveChanges();//сохранение
+                MessageBox.Show("Данные успешно добавлены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при добавлении данных!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
